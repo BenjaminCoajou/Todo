0eeba8db8cea16c04b656b634f35af9fffabd1af
BenjaminCoajou <coajou.benjamin@gmail.com>
2021-09-23 14:19:27 +0200

    add component filter and html in filter and tasks component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './App.css';
 import NewTask from '../NewTask';
 import Tasks from '../Tasks';
+import Filter from '../Filter';
 
 const App = () => (
     <div className="App m-auto">
       <NewTask/>
+      <Filter/>
       <Tasks/>
     </div>
   );
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -4,8 +4,18 @@ import './style.css';
 
 const Tasks = () => (
     <div className="Tasks">
-      my tasks
+      <h2 className="mb-4">My Tasks</h2>
+      <div className="card flex-row align-items-baseline mb-4">
+        <div className="card-body">
+          Do something interesting !!
+        </div>
+
+        <div className="card-button d-flex align-self-center">
+        <button type="button" className="btn btn-success mx-1 d-flex align-items-center"><span className="material-icons">check_circle</span></button>
+        <button type="button" className="btn btn-primary mx-1 d-flex align-items-center"><span className="material-icons">edit</span></button>
+        <button type="button" className="btn btn-danger mx-1 d-flex align-items-center"><span className="material-icons">delete</span></button>
+        </div>
+      </div>
     </div>
   );
-
 export default Tasks;
