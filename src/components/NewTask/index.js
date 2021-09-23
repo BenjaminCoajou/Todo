487355a120f77c7e8cd0170b6db143c916f95c36
BenjaminCoajou <coajou.benjamin@gmail.com>
2021-09-23 13:18:17 +0200

    add components tasks and newtask

---
+++ src/components/NewTask/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+const NewTask = () => (
+    <div className="NewTask">
+      add task
+    </div>
+  );
+
+export default NewTask;
---
+++ src/components/Tasks/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+const Tasks = () => (
+    <div className="Tasks">
+      my tasks
+    </div>
+  );
+
+export default Tasks;
